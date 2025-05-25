1e29a3d2dc20ce1cd303b43e6f18b56f85a9a011
hendritjipto <8566170+pix3lize@users.noreply.github.com>
2025-05-25 08:39:45 +0700

    Add mongod, mongotop, and mongostat to pix3lize/mdbubuntu image

--- init-replica.sh
+++ init-replica.sh
@@ -16,37 +16,64 @@ rs.initiate(
             {
                 _id: 1,
                 host: "mongoserver2:27017"
-            },
+            }
+            ,
             {
                 _id: 2,
                 host: "mongoserver3:27017"
-            },
-            {
-                _id: 3,
-                host: "mongoserver4:27017"
-            },
-            {
-                _id: 4,
-                host: "mongoserver5:27017"
             }
         ]
     }
-)
+ )
+# rs.initiate(
+#     {
+#         _id: "mongo-replica",
+#         members: [
+#             {
+#                 _id: 0,
+#                 host: "mongoserver1:27017"
+#             },
+#             {
+#                 _id: 1,
+#                 host: "mongoserver2:27017"
+#             },
+#             {
+#                 _id: 2,
+#                 host: "mongoserver3:27017"
+#             },
+#             {
+#                 _id: 3,
+#                 host: "mongoserver4:27017"
+#             },
+#             {
+#                 _id: 4,
+#                 host: "mongoserver5:27017"
+#             }
+#         ]
+#     }
+# )
 
 // Wait for the replica set to initialize
 sleep(10000)
 
 // Add the aribiter 
-rs.addArb("mongoserver6:27017")
+//rs.addArb("mongoserver6:27017")
 
 // Change the priority of the nodes
 cfg = rs.conf()
 cfg.members[0].priority = 8
 cfg.members[1].priority = 7
 cfg.members[2].priority = 6
-cfg.members[3].priority = 5
-cfg.members[4].priority = 5
 rs.reconfig(cfg, { force: true })
+
+// Change the priority of the nodes
+# cfg = rs.conf()
+# cfg.members[0].priority = 8
+# cfg.members[1].priority = 7
+# cfg.members[2].priority = 6
+# cfg.members[3].priority = 5
+# cfg.members[4].priority = 5
+# rs.reconfig(cfg, { force: true })
 
 // Print the final configuration
 printjson(rs.conf())
